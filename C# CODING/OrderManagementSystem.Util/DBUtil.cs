c8515d404f96db2e86380d7651d5548288cef86c
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-15 12:36:27 +0530

    Add files via upload

---
+++ C# CODING/OrderManagementSystem.Util/DBUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+    namespace OrderManagementSystem.Util
+    {
+        public static class DBUtil
+        {
+
+            public static SqlConnection getDBConnection()
+            {
+                
+            string connString = "Data Source=SRIKEERTHANA;Initial Catalog=oms;Integrated Security=True;TrustServerCertificate=True";
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            return conn;
+
+        }
+        
+    }
+    }
+
+
