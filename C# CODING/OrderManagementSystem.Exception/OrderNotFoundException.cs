4af02b5289a3c04d708ba20c1839959c1d09bfe7
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-15 12:37:07 +0530

    Add files via upload

---
+++ C# CODING/OrderManagementSystem.Exception/OrderNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Exception
+{
+    public class OrderNotFoundException : System.Exception
+    {
+        public OrderNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ C# CODING/OrderManagementSystem.Exception/UserNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Exception
+{
+    public class UserNotFoundException : System.Exception
+    {
+        public UserNotFoundException(string message) : base(message) { }
+    }
+}
+
