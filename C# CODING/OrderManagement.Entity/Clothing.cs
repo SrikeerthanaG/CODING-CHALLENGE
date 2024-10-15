222e058be70142f4393936b53a48a16eac2ada93
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-15 12:34:01 +0530

    Add files via upload

---
+++ C# CODING/OrderManagement.Entity/Clothing.cs
@@ -0,0 +1,22 @@
+﻿using OrderManagementSystem.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Entity
+{
+    public class Clothing : Product
+    {
+        public string Size { get; set; }
+        public string Color { get; set; }
+
+        public Clothing(int productId, string productName, string description, double price, int quantityInStock, string size, string color)
+            : base(productId, productName, description, price, quantityInStock, "Clothing")
+        {
+            Size = size;
+            Color = color;
+        }
+    }
+}
